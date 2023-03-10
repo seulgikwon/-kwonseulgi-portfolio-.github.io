82dd653098b61a6d3cc4153c5dc2aa97d7dc231d
seulgikwon <127458379+seulgikwon@users.noreply.github.com>
2023-03-10 14:57:12 +0900

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.sns_slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: false,
+      });
+});
